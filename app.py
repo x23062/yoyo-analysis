2ceea78e90dc59d4e52cec7c0555a5c5d29ea33e
x23062 <x23062xx@aitech.ac.jp>
2026-04-21 16:12:19 +0900

    元に戻したよ

--- app.py
+++ app.py
@@ -1368,8 +1368,8 @@ def get_results_user_graph():
 
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, threaded=True)
-
 # if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=5000, threaded=True, debug=True)
+#     app.run(host="0.0.0.0", port=8000, threaded=True)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, threaded=True, debug=True)
