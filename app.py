826a08a6bde12cd63070fc547361006abe5a216e
x23062 <x23062xx@aitech.ac.jp>
2026-04-21 19:55:27 +0900

    localhostから変更

--- app.py
+++ app.py
@@ -1372,4 +1372,4 @@ def get_results_user_graph():
 #     app.run(host="0.0.0.0", port=8000, threaded=True)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
